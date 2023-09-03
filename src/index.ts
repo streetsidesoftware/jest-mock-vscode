78b744bf61d7689d7c9fe01c014a47995e555281
Jason Dent <jason@streetsidesoftware.nl>
2023-09-03 16:39:50 +0200

    export needed methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-export { createVSCodeMock } from './vscode-mock';
-export { createTextDocument, readTextDocument } from './vscodeTypesHelper';
-export type * from './vscode';
 export type { TestFramework } from './TestFramework';
 export type { Mock } from './TestFrameworkTypes';
+export {
+    createLanguages,
+    createMockFileSystem,
+    createMockFileSystemProvider,
+    createMockWorkspaceConfiguration,
+} from './vscode';
+export type { MockWorkspaceConfigurationData } from './vscode';
+export { createVSCodeMock } from './vscode-mock';
+export { createTextDocument, readTextDocument } from './vscodeTypesHelper';
