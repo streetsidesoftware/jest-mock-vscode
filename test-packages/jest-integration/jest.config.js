4944992a5684a82f6ff0f50ca937c817e13984a5
Jason Dent <Jason3S@users.noreply.github.com>
2023-09-04 11:01:02 +0200

    test: Add integration test for jest (#121)

--- test-packages/jest-integration/jest.config.js
+++ test-packages/jest-integration/jest.config.js
@@ -3,8 +3,7 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testMatch: ['**/*.test.ts'],
-    testPathIgnorePatterns: ['/node_modules/', '/test-packages/'],
+    testMatch: ['**/*.+(spec|test).[tj]s?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     // Use 30sec as a default timeout
     testTimeout: 30000,
